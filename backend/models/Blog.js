fd4253afd566e8d4389f1b98edee88dc9d8ba181
Tafadzwa Munyuki <35223331+tafadzwamunyuki@users.noreply.github.com>
2025-04-27 19:03:03 +0100

    Update Blog.js

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -1,11 +1,29 @@
 import mongoose from 'mongoose';
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  author: String,
-  image: String,
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  publishedAt: {
+    type: Date,
+  },
 }, { timestamps: true });
+
+const Blog = mongoose.model("Blog", blogSchema)
+
 
 
 export default mongoose.model('Blog', blogSchema);
